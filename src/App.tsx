656b2516b74d23b9b12fdb4cdaedab0573e9beda
dtsai1022 <110254751+dtsai1022@users.noreply.github.com>
2022-07-30 22:18:08 -0700

    I love my updated navbar!

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,19 @@
 import React from 'react';
 import './App.css';
-import { BrowserRouter as Router} from 'react-router-dom';
+import { BrowserRouter as Router, Switch, Route} from 'react-router-dom';
 import NavigationBar from './components/NavigationBar';
+import Home from './pages';
+import foodTracker from './pages';
 
 const App: React.FC = () => {
   
   return (
     <Router>
       <NavigationBar />
-      <h1>Project Baymax</h1>
+
     </Router>
   );
 }
+
 
 export default App;
--- src/components/NavigationBar.tsx
+++ src/components/NavigationBar.tsx
@@ -5,26 +5,24 @@ const NavigationBar = () => {
     return (
         <>
             <Nav>
-                <NavLink to="/">
-
+                <NavLink to='/'>
+                    <h2>Home</h2>
                 </NavLink>
                 <Bars />
                 <NavMenu>
-                    <NavLink to="/food-tracker" activeStyle>
+                    <NavLink to='/food-tracker' >
                         <h2>Food Tracker</h2>
                     </NavLink>
-                    <NavLink to="/health-tracker" activeStyle>
+                    <NavLink to="/health-tracker" >
                         <h2>Health Tracker</h2>
                     </NavLink>
-                    <NavLink to="/substance-quitter" activeStyle>
+                    <NavLink to="/substance-quitter" >
                         <h2>Subtance Quitter</h2>
-                    </NavLink>
-                    <NavLink to="/sign-up" activeStyle>
-                        <h2>Sign Up</h2>
                     </NavLink>
                 </NavMenu>
                 <NavBtn>
                     <NavBtnLink to="/sign-in">Sign In</NavBtnLink>
+                    <NavBtnLink to="/sign-up">Sign up</NavBtnLink>
                 </NavBtn>
             </Nav>
         </>
