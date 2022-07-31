585bf3935e8a04ed4d06e64ab5fa04577a7769de
rhung192 <110218331+rhung192@users.noreply.github.com>
2022-07-30 19:29:35 -0700

    Add files via upload

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import './App.css';
+import { BrowserRouter as Router} from 'react-router-dom';
+import NavigationBar from './components/NavigationBar';
+
+const App: React.FC = () => {
+  
+  return (
+    <Router>
+      <NavigationBar />
+      <h1>Project Baymax</h1>
+    </Router>
+  );
+}
+
+export default App;
